9806b392649695464d7689434fc5d3d2fd14fc01
Unkn8n <immanuelser3@gamil.com>
2026-05-16 01:12:32 +0800

    Fresh deployment clean slate workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "unkn8n.github.io/Notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
